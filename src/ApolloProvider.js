1f2ede80eb4e5b064e89b13961379ccedf55b639
gjgit <gjramanaa@gmail.com>
2020-09-27 23:17:38 +0530

    checking .gitnore

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
   uri: 'https://subhashserver.herokuapp.com/'
-  //uri: 'http://localhost:5000/sss'
+  //uri: 'http://localhost:5000/'
 });
 
 const authLink = setContext(() => {
